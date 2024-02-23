fe001bc88aae6df102e89a07b4fc2dfbf8bb8ded
UDAYVEERUS <udayveerus348567@gmail.com>
2024-02-23 22:47:52 +0530

    initial commit

---
+++ src/component/AboutUs.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function AboutUs() {
+  return (
+    <div className="about-us">
+      <div>
+        <div>
+          <div>
+            <h1>About Us</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget enim sit amet lectus dapibus malesuada. Ut feugiat turpis id ante luctus, ac eleifend felis hendrerit.</p>
+            <p>Integer rhoncus ipsum in lacus malesuada, id scelerisque nisi rhoncus. Fusce volutpat nulla at metus tincidunt, a tristique libero interdum.</p>
+            <p>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Cras sit amet sapien in leo fermentum consequat. Nam sodales ligula non justo consequat varius.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AboutUs;
